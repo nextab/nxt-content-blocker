9458e858ac23f53e9442484a2914cf94938fa52f
Benedikt Braut <benedikt.braut@outlook.de>
2023-10-11 09:56:21 +0200

    Minor JS Changes

--- js/nxt-content-blocker-frontend.js
+++ js/nxt-content-blocker-frontend.js
@@ -1,23 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-	var agreementButtons = document.querySelectorAll(".agreement-button");
-	
-	agreementButtons.forEach(function(button) {
-		button.addEventListener("click", function() {
-			// Set the cookie as before
-			document.cookie = "nxt-content-agreement=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-			
-			// Get the overlay that's parent to the clicked button
-			var overlay = button.closest('.agreement-overlay');
-			
-			// Assuming you store the actual content as data-content attribute
-			var actualContent = overlay.getAttribute('data-content');
-			
-			// Replace the overlay with the actual content
-			if (actualContent) {
-				overlay.outerHTML = actualContent;
-			} else {
-				overlay.style.display = "none";
-			}
-		});
-	});
-});
+document.addEventListener('DOMContentLoaded', function () {
+  const agreementButtons = document.querySelectorAll('.agreement-button')
+
+  agreementButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      // Set the cookie as before
+      document.cookie =
+        'nxt-content-agreement=true; expires=Fri, 31 Dec 9999 23:59:59 GMT'
+
+      // Get the overlay that's parent to the clicked button
+      const overlay = button.closest('.agreement-overlay')
+
+      // Assuming you store the actual content as data-content attribute
+      const actualContent = overlay.dataset.content
+
+      // Replace the overlay with the actual content
+      if (actualContent) {
+        overlay.outerHTML = actualContent
+      } else {
+        overlay.style.display = 'none'
+      }
+    })
+  })
+})
